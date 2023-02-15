aabc0f978e55d166a748bda028de222fca891fb9
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-02-15 23:38:20 +0200

    Add HW task 1-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime(time) {
+  localStorage.setItem(CURRENT_TIME, Math.round(time.seconds));
+}
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  inputEmail: document.querySelector('input[name="email"]'),
+  inputTextarea: document.querySelector('textarea[name="message"]'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+getItems();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
+
+function onFormInput() {
+  formData.email = refs.inputEmail.value;
+  formData.message = refs.inputTextarea.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function getItems() {
+  const savedItems = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedItems) {
+    refs.inputEmail.value = savedItems.email;
+    refs.inputTextarea.value = savedItems.message;
+  }
+}
