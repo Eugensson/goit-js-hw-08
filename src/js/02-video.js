e09d2f1ea65a3a29a37952dd5a630c381d15466c
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-02-17 11:45:40 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,9 +6,9 @@ const CURRENT_TIME = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
+player.on('play', function () {
+  console.log('played the video!');
+});
 
 // player.getVideoTitle().then(function (title) {
 //   console.log('title:', title);
