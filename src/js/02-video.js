97b79ca46c7b280ca638acf4eacb3e501bb341bf
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-02-17 14:39:23 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,15 @@
 import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
-const currentTime = localStorage.getItem('videoplayer-current-time');
+const currentTime = localStorage.getItem(STORAGE_KEY);
 if (currentTime) player.setCurrentTime(currentTime);
 
 const saveCurrentTime = function (time) {
-  localStorage.setItem('videoplayer-current-time', Math.round(time.seconds));
+  localStorage.setItem(STORAGE_KEY, Math.round(time.seconds));
 };
 
 player.on('timeupdate', throttle(saveCurrentTime, 1000));
